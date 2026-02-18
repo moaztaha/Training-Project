06abddbc899ada76e4a14eb2ad2ec72c1edb5e64
Moaz Taha <moaztaha62@gmail.com>
2026-02-18 20:50:01 +0200

    Feat: Add ServicesSection Component

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "@/components/organisms/AboutSection";
 import HeroSection from "@/components/organisms/HeroSection";
+import ServicesSection from "@/components/organisms/ServicesSection";
 
 export default function page() {
   return (
     <div>
       <HeroSection />
       <AboutSection/>
+      <ServicesSection/>
     </div>
   );
 }
