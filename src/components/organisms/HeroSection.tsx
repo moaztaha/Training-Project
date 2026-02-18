2043fd5cbec890a190d6daf254bf3021d07714eb
Moaz Taha <moaztaha62@gmail.com>
2026-02-18 20:43:57 +0200

    Feat: Add HeroSection Component

---
+++ src/components/organisms/HeroSection.tsx
@@ -0,0 +1,15 @@
+import Text from "@/components/atoms/Text";
+import Title from "@/components/atoms/Title";
+export default function HeroSection() {
+  return (
+    <div className="ds-bg p-7 flex justify-center flex-col items-center">
+      <Title>HeroSection</Title>
+      <Text size="md">
+        This section represents the Hero Section of the website.
+      </Text>
+      <Text size="md" className="text-green-400">
+        created by : Moaz Taha
+      </Text>
+    </div>
+  );
+}
